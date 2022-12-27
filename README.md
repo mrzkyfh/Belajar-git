d0d1e0420a220ec26f0853c95207491838bd7d97
mrzkyfh <mrzkyfh@gmail.com>
2022-12-27 15:00:29 +0700

    hapus

--- README.md
+++
@@ -1,6 +0,0 @@
-How to deploy dumbflix-frontend :
-- Install node.js 10.x
-- Change url for dumbflix-backend in src/config/api.js
-- Deploy dumbflix-frontend with port 3000
-
-Test CICD
--- src/redux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux"; //Root Reducers
-
-import film from "./film";
-import auth from "./auth";
-import payment from "./payment";
-
-export default combineReducers({ film, auth, payment });
